71bbf60dd4f8a46ecac59d561ffd1949102fdaf0
James Cicenia <james@jimijon.com>
2019-08-29 21:46:26 -0400

    init project

---
+++ SwiftUI-SQL-ExpMaker/SwiftUI-SQL-ExpMaker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SQL-ExpMaker
+//
+//  Created by James Cicenia on 8/29/19.
+//  Copyright © 2019 James Cicenia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
